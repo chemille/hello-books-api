01475f40f68e99bfc30d7fac4dd3beb8c4d1ce37
Michelle Oh <ohmotrl@gmail.com>
2022-10-22 11:45:52 -0400

    refactored validate_book function to handle 400 and 404 errors

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -12,26 +12,31 @@ books = [
     Book(3, "The Snowy Day", "Caldecott Medal winner for beautiful illustrations")
 ] 
 
-hello_world_bp = Blueprint("hello_world", __name__)
-
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
+# # helper fx
+def validate_book(book_id):
+    # handle invalid book_id, return 400
     try:
         book_id = int(book_id) 
     except:
-        return {"message": f"book {book_id} invalid"}, 400
-        # use try and except to handle invalid errors 
+        abort(make_response({"message": f"book {book_id} invalid"}, 400))
+    # search for book_id in data, return book
     for book in books:
         if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }
-            
-    return {"message": f"book {book_id} not found"}, 404
+            return book    
+    # return a 404 for non-existing book
+    abort(make_response({"message": f"book {book_id} not found"}, 404))
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id) # call helper fx
+    # now this fx only does one thing: print out json that we want
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
 
 @books_bp.route("", methods=["GET"])
 def handle_books():
@@ -43,29 +48,3 @@ def handle_books():
             "description": book.description
         })   
     return jsonify(books_response), 200
-
-@hello_world_bp.route("/hello-world", methods=["GET"]    
-)
-def say_hello_world():
-    my_beautiful_world = "Hello, World!"
-    return my_beautiful_world, 200
-
-@hello_world_bp.route("/hello/JSON", methods=["GET"])
-def say_hello_json():
-    return {
-        "name": "Chemille",
-        "message": "Need more coffee!", 
-        "hobbies": ["Snacks", "Coding", "Cooking"]
-    }, 200
-    
-@hello_world_bp.route("/broken-endpoint-with-broken-server-code", methods=["GET"])
-def broken_endpoint():
-    response_body = {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-    
-    new_hobby = "Surfing"
-    response_body["hobbies"].append(new_hobby)
-    return response_body
