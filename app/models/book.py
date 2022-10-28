ca4e301147321808e7e9a60783d3ec0efa7d2cb4
Michelle Oh <ohmotrl@gmail.com>
2022-10-28 08:42:41 -0400

    followed learn lesson 03 Building an API db, models, create, and read

--- app/models/book.py
+++ app/models/book.py
@@ -1,11 +1,10 @@
 from app import db
 
 
-class Book(db.Model): # this tells SQLAlchemy this is a book class which is a model
-    # and it has these 3 columns
+class Book(db.Model): 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     
-    def to_string(self):
-        return f"{self.id}: {self.title} Description: {self.description}"
+    # def to_string(self):
+    #     return f"{self.id}: {self.title} Description: {self.description}"
--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,33 @@
 from app import db 
 from app.models.book import Book
-from flask import Blueprint, jsonify, abort, make_response, request
+from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["POST"])
-def handle_books():
-    request_body = request.get_json()
-    if "title" not in request_body or "description" not in request_body:
-        return make_response("Invalid Request", 400)
+@books_bp.route("", methods=["GET"])
+def read_all_books():
+    books = Book.query.all() 
+    books_response = [] 
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })   
+    return jsonify(books_response)
     
+@books_bp.route("", methods=["POST"])
+def create_book():
+    request_body = request.get_json()    
     new_book = Book(
         title = request_body["title"],
         description = request_body["description"]
     )
+    
     db.session.add(new_book)
     db.session.commit()
     
-    return f"Book {new_book.title} created", 201
-
+    return make_response(f"Book {new_book.title} successfully created", 201)
 
 # class Book:
 #     def __init__(self, id, title, description):
@@ -57,13 +66,3 @@ def handle_books():
 #         "description": book.description
 #     }
 
-# @books_bp.route("", methods=["GET"])
-# def handle_books():
-#     books_response = [] 
-#     for book in books:
-#         books_response.append({
-#             "id": book.id,
-#             "title": book.title,
-#             "description": book.description
-#         })   
-#     return jsonify(books_response), 200
