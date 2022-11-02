66059a9e59bbfdc0c22de036d6d306f4c4dc849b
Michelle Oh <ohmotrl@gmail.com>
2022-11-02 11:54:24 -0400

    added methods for query params and refactored read_all_books function and create_book function

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,40 @@
 from app import db 
 from app.models.book import Book
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+
+### helper fxs
+def validate_book(book_id):
+    # handle invalid book_id, return 400
+    try:
+        book_id = int(book_id) 
+    except:
+        abort(make_response({"message": f"book {book_id} invalid"}, 400))
+    
+    book = Book.query.get(book_id) # SQLAlchemy syntax to query for one Book resource. Returns an instance of Book.
+    # This get method returns None if book is not found, so we need to handle it below
+    
+    if not book:
+        abort(make_response({"message": f"book {book_id} not found"}, 404))
+        
+    return book # If book is truthy and found, it will return it and be stored in validate_book
+
+## route fxs
 @books_bp.route("", methods=["GET"])
-def read_all_books():
-    books = Book.query.all() 
+def read_all_books():        
     books_response = [] 
+    
+    title_query = request.args.get("title") # this gets key-value pair in url 
+    
+    if title_query is not None:
+        books = Book.query.filter_by(title=title_query) #keyword arg
+        # so it's going to look for a field in our table called title and 
+        # it's going to get all the books that match this title
+    else:
+        books = Book.query.all() 
+        
     for book in books:
         books_response.append({
             "id": book.id,
@@ -29,40 +56,34 @@ def create_book():
     
     return make_response(f"Book {new_book.title} successfully created", 201)
 
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
-        
-# books = [
-#     Book(1, "The Giving Tree", "One of Shel Silverstein's many books"),
-#     Book(2, "The Very Hungry Caterpillar", "Popular children's book"),
-#     Book(3, "The Snowy Day", "Caldecott Medal winner for beautiful illustrations")
-# ] 
-
-
-# # # helper fx
-# def validate_book(book_id):
-#     # handle invalid book_id, return 400
-#     try:
-#         book_id = int(book_id) 
-#     except:
-#         abort(make_response({"message": f"book {book_id} invalid"}, 400))
-#     # search for book_id in data, return book
-#     for book in books:
-#         if book.id == book_id:
-#             return book    
-#     # return a 404 for non-existing book
-#     abort(make_response({"message": f"book {book_id} not found"}, 404))
-
-# @books_bp.route("/<book_id>", methods=["GET"])
-# def handle_book(book_id):
-#     book = validate_book(book_id) # call helper fx
+@books_bp.route("/<book_id>", methods=["GET"])
+def read_one_book(book_id): # this fx is called whenever the HTTP reqest matches the decorator
+    book = validate_book(book_id) # call helper fx
 #     # now this fx only does one thing: print out json that we want
-#     return {
-#         "id": book.id,
-#         "title": book.title,
-#         "description": book.description
-#     }
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
 
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id) # use helper fx
+    request_body = request.get_json()    
+    
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+    # we never want to update an id. It's covered by postgreSQL
+    
+    db.session.commit() # this commits the change that we made so that it persists in the db
+    
+    return make_response(f"Book #{book_id} successfully updated")
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id) # use helper fx   
+    
+    db.session.delete(book)
+    db.session.commit()
+    
+    return make_response(f"Book #{book_id} successfully deleted")
